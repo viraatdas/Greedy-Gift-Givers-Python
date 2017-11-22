f5ad209c5d36d253552dc02428baca0bdaca9aec
Viraat <viraat.laldas@gmail.com>
2017-11-22 16:17:20 -0500

    completed code - tested and works

--- GreedyGiftGivers.py
+++ GreedyGiftGivers.py
@@ -5,8 +5,8 @@ TASK: gift1
 """
 
 
-fin = open ('gift1.txt', 'r')
-fout = open ('gift2.txt', 'w')
+fin = open ('gift1.in', 'r')
+fout = open ('gift1.out', 'w')
 
 a = int (fin.readline())
 
@@ -15,126 +15,47 @@ for i in range(a):
     name.append(fin.readline())
 
 
-with open('gift1.txt') as f:
+with open('gift1.in') as f:
    size=sum(1 for _ in f)
 
+print (size)
 money= [0]*a
 
-p = a+2
 
 
 
-pg = fin.readline() #person who is giving
+temp = 0
 
-for i in range(a):
-    if(name[i] == pg):
-        mon = fin.readline()
-        p = p+1
-        mon = mon.split()
-        tot = int(mon[0])
-        peo = int(mon[1])
-        money[i] = money[i] - peo*int(tot/peo)
-        for _ in range(peo):
-            pr = fin.readline() #person who is receiving
-            op= -1
-            while (op<a-1):
-                op=op+1
-                if (pr == name[op]):
-                    money[op] = money[op]+int(tot/peo)
+while temp <size-a:
 
+    pg = fin.readline() #person who is giving
+    temp = temp+1
 
+    for i in range(a):
+        if(name[i] == pg):
+            mon = fin.readline()
+            temp=temp+1
+            mon = mon.split()
+            tot = int(mon[0])
+            peo = int(mon[1])
+            if peo==0:
+                break
+            money[i] = money[i] - peo*int(tot/peo)
+            for _ in range(peo):
+                pr = fin.readline() #person who is receiving
+                temp = temp + 1
+                op= -1
+                while (op<a-1):
+                    op=op+1
+                    if (pr == name[op]):
+                        money[op] = money[op]+int(tot/peo)
 
-
-pg = pr  #person who is giving
-
-print (money)
-
-for i in range(a):
-    if(name[i] == pg):
-        mon = fin.readline()
-        p = p+1
-        mon = mon.split()
-        tot = int(mon[0])
-        peo = int(mon[1])
-        money[i] = money[i] - peo*int(tot/peo)
-        for _ in range(peo):
-            pr = fin.readline() #person who is receiving
-            op= -1
-            while (op<a-1):
-                op=op+1
-                if (pr == name[op]):
-                    money[op] = money[op]+int(tot/peo)
-
-
-
-
-pg = pr
-
-for i in range(a):
-    if(name[i] == pg):
-        mon = fin.readline()
-        p = p+1
-        mon = mon.split()
-        tot = int(mon[0])
-        peo = int(mon[1])
-        money[i] = money[i] - peo*int(tot/peo)
-        for _ in range(peo):
-            pr = fin.readline() #person who is receiving
-            op= -1
-            while (op<a-1):
-                op=op+1
-                if (pr == name[op]):
-                    money[op] = money[op]+int(tot/peo)
 
 
 
 print (money)
-pg = fin.readline()
-
-
+for ti in range(a):
+    name[ti] = name[ti].strip('\n')
 for i in range(a):
-    if(name[i] == pg):
-        mon = fin.readline()
-        p = p+1
-        mon = mon.split()
-        tot = int(mon[0])
-        peo = int(mon[1])
-        money[i] = money[i] - peo*int(tot/peo)
-        for _ in range(peo):
-            pr = fin.readline() #person who is receiving
-            op= -1
-            while (op<a-1):
-                op=op+1
-                if (pr == name[op]):
-                    money[op] = money[op]+int(tot/peo)
-
-
-
-pg=fin.readline()
-
-
-
-for i in range(a):
-    if(name[i] == pg):
-        mon = fin.readline()
-        p = p+1
-        mon = mon.split()
-        tot = int(mon[0])
-        peo = int(mon[1])
-        money[i] = money[i] - peo*int(tot/peo)
-        for _ in range(peo):
-            pr = fin.readline() #person who is receiving
-            op= -1
-            while (op<a-1):
-                op=op+1
-                if (pr == name[op]):
-                    money[op] = money[op]+int(tot/peo)
-
-
-
-
-print (pg)
-
-
-print (money)
+    fout.write(str(name[i])+ " "+ str(money[i]) + "\n")
 
